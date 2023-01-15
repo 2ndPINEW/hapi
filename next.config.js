e78704b6487865c1457d13afe8ad0a0bb8441350
Eito Takase <eito01178@gmail.com>
2023-01-15 15:00:10 +0900

    appDir動かなかったからやめた

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    runtime: "experimental-edge",
-  },
+  reactStrictMode: true,
+  runtime: "experimental-edge",
 }
 
 module.exports = nextConfig
